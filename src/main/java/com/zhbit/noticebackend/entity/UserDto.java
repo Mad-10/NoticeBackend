0afef16650c894948d76e5df5883d5a45138640e
Alan Ivan <2728946156@qq.com>
2023-04-21 15:51:03 +0800

    将手动添加Setter和getter改为添加lombok中的@Data注解。

--- src/main/java/com/zhbit/noticebackend/entity/UserDto.java
+++ src/main/java/com/zhbit/noticebackend/entity/UserDto.java
@@ -1,34 +1,12 @@
 package com.zhbit.noticebackend.entity;
 
+import lombok.Data;
+
+@Data
 public class UserDto {
     private Long id;
     private String username;
     private String password;
     private String message;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
