63efa20fa879b3f2a9cf18a8ad7b1fa0f4cba711
Alan Ivan <2728946156@qq.com>
2023-04-21 10:49:35 +0800

    以默认消息创建无参构造函数

--- src/main/java/com/zhbit/noticebackend/exception/UserAlreadyExistsException.java
+++ src/main/java/com/zhbit/noticebackend/exception/UserAlreadyExistsException.java
@@ -1,6 +1,10 @@
 package com.zhbit.noticebackend.exception;
 
 public class UserAlreadyExistsException extends Exception {
+    public UserAlreadyExistsException() {
+        super("Username already exists");
+    }
+
     public UserAlreadyExistsException(String message) {
         super(message);
     }
--- src/main/java/com/zhbit/noticebackend/exception/UserNotFoundException.java
+++ src/main/java/com/zhbit/noticebackend/exception/UserNotFoundException.java
@@ -1,6 +1,9 @@
 package com.zhbit.noticebackend.exception;
 
 public class UserNotFoundException extends Exception {
+    public UserNotFoundException() {
+        super("User has not been registered.");
+    }
     public UserNotFoundException(String message) {
         super(message);
     }
