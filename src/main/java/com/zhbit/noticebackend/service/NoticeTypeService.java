cd7633201f6fb599e0dc79497ec4f7c3b0eae753
Alan Ivan <2728946156@qq.com>
2023-04-21 14:41:48 +0800

    在NoticeTypeService中添加curd方法。

--- src/main/java/com/zhbit/noticebackend/service/NoticeTypeService.java
+++ src/main/java/com/zhbit/noticebackend/service/NoticeTypeService.java
@@ -1,21 +1,53 @@
 package com.zhbit.noticebackend.service;
 
+import com.zhbit.noticebackend.entity.NoticeType;
 import com.zhbit.noticebackend.mapper.NoticeRecordMapper;
 import com.zhbit.noticebackend.mapper.NoticeTypeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class NoticeTypeService {
-    @Autowired
+
+
     private final NoticeTypeMapper noticeTypeMapper;
 
-    @Autowired
     private final NoticeRecordMapper noticeRecordMapper;
 
+    @Autowired
     public NoticeTypeService(NoticeTypeMapper noticeTypeMapper, NoticeRecordMapper noticeRecordMapper) {
         this.noticeTypeMapper = noticeTypeMapper;
         this.noticeRecordMapper = noticeRecordMapper;
+    }
+
+    public NoticeType findById(Integer id) {
+        return noticeTypeMapper.findById(id);
+    }
+
+    public NoticeType findByTypeName(String typeName) {
+        return noticeTypeMapper.findByTypeName(typeName);
+    }
+
+    public List<NoticeType> findAll() {
+        return noticeTypeMapper.findAll();
+    }
+
+    public void save(NoticeType noticeType) {
+        noticeTypeMapper.save(noticeType);
+    }
+
+    public void update(NoticeType noticeType) {
+        noticeTypeMapper.update(noticeType);
+    }
+
+    public void deleteById(Integer id) {
+        noticeTypeMapper.deleteById(id);
+    }
+
+    public void deleteByTypeName(String typeName) {
+        noticeTypeMapper.deleteByTypeName(typeName);
     }
 
 }
